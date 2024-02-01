f1df009f635d5bd744d04154b04b7da29f4e4cfc
s-ellingwood <136346014+s-ellingwood@users.noreply.github.com>
2024-02-01 18:58:17 -0500

    Upload .js files

---
+++ Resources/js_files/position_performance.js
@@ -0,0 +1,37 @@
+let position_data = {
+    "minutes_played": {
+        "point guard": 23.986077599772205,
+        "shooting guard": 24.169656280524922,
+        "small forward": 24.157094194312794,
+        "center": 20.8557508667356,
+        "power forward": 22.677245087371183
+    },
+    "field_goal_rate": {
+        "point guard": 42.79490139523336,
+        "shooting guard": 43.036349639283124,
+        "small forward": 44.214111552622896,
+        "center": 50.805971862422325,
+        "power forward": 46.8047017502737
+    },
+    "rebound_rate": {
+        "point guard": 2.5888328562650798,
+        "shooting guard": 2.8497961466045667,
+        "small forward": 3.796153966076328,
+        "center": 5.825515479593699,
+        "power forward": 5.270060523302538
+    },
+    "free_throw_rate": {
+        "point guard": 80.21057127000482,
+        "shooting guard": 80.12115066362033,
+        "small forward": 77.19621607970808,
+        "center": 68.09041820848589,
+        "power forward": 73.08058723116187
+    },
+    "total_points": {
+        "point guard": 9.95047207277414,
+        "shooting guard": 10.540156995427456,
+        "small forward": 9.936531086305811,
+        "center": 8.130070403260142,
+        "power forward": 9.351806809049336
+    }
+}
